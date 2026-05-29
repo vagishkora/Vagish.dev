3ed0e3e0a0ae9f2fb79f42d919b465d8d36831bf
vagishkora <vagishkora@gmai.com>
2026-05-29 21:17:09 +0530

    fix: resolve cursor type error

--- src/components/ui/inverted-cursor.tsx
+++ src/components/ui/inverted-cursor.tsx
@@ -8,7 +8,7 @@ interface CursorProps {
 
 export const Cursor: React.FC<CursorProps> = ({ size = 60 }) => {
   const cursorRef = useRef<HTMLDivElement>(null);
-  const requestRef = useRef<number>();
+  const requestRef = useRef<number>(0);
   const previousPos = useRef({ x: -size, y: -size }); // start off-screen
   
   const [visible, setVisible] = useState(false);
